2c88f2b9f15e400a817a1abe9be417fefb2982c1
ValeriiSielikhov <valeriy.selikhovv@gmail.com>
2021-05-25 22:04:59 +0300

    task 13

---
+++ scripts/tasks/task_13.sql
@@ -0,0 +1,29 @@
+USE theater; 
+
+
+DROP PROCEDURE IF EXISTS place;
+delimiter $$
+CREATE PROCEDURE place()
+	BEGIN
+		SELECT stagings.name_ AS 'name', (20 - COUNT(tickets.id_performance)) AS 'free place', performances.date_ AS 'date'
+		FROM tickets, performances, stagings
+		WHERE performances.id_staging = stagings.id_staging AND performances.id_performance = tickets.id_performance
+		GROUP BY tickets.id_performance
+        UNION
+		SELECT 'Total:' , (COUNT(DISTINCT date_)*20-COUNT(id_ticket)),'' FROM tickets; 
+	END$$
+delimiter ;
+CALL place();
+
+
+DROP PROCEDURE IF EXISTS place_performance;
+delimiter $$
+CREATE PROCEDURE place_performance(IN t TEXT) 
+	BEGIN
+		SELECT t as 'name', (20 - COUNT(tickets.id_performance)) AS 'free place',
+		performances.date_ AS 'date' FROM tickets, performances, stagings
+		WHERE stagings.name_ = t AND performances.id_staging = stagings.id_staging AND performances.id_performance = tickets.id_performance
+		GROUP BY tickets.id_performance;
+	END$$
+delimiter ;
+CALL place_performance('A Streetcar Named Desire');
